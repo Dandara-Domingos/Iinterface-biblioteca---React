39b54791ab0643cf228f01ad5de26bf7217e28d2
Dandara Gabrielli <dandaragabrielle00@gmail.com>
2025-04-09 08:56:20 -0300

    Versionando o Desafio novamente

--- src/pages/PHome/PHome.tsx
+++ src/pages/PHome/PHome.tsx
@@ -1,18 +1,30 @@
-import Cabecalho from "../../components/Cabecalho/Cabecalho";
-import LoginForm from "../../components/LoginForm/LoginForm";
-import Rodape from "../../components/Rodape/Rodape";
-import TabelaAluno from "../../components/Tabelas/TabelaAluno/TabelaAluno";
-import TabelaEmprestimo from "../../components/Tabelas/TabelaEmprestimo/TabelaEmprestimo";
-import TabelaLivro from "../../components/Tabelas/TabelaLivro/TabelaLivro";
+// Importa o tipo JSX do React para tipar corretamente o retorno do componente
+import { JSX } from 'react';
 
-function PHome() {
+// Importa o componente de cabeçalho da aplicação
+import Cabecalho from "../../components/Cabecalho/Cabecalho";
+
+// Importa o componente de boas-vindas
+import Welcome from "../../components/Welcome/Welcome";
+
+// Importa o componente de rodapé da aplicação
+import Rodape from "../../components/Rodape/Rodape";
+
+// Declara o componente funcional PHome, que representa a página inicial
+function PHome(): JSX.Element {
     return (
-        <>
-        <Cabecalho/>
-        <TabelaEmprestimo/>
-        <Rodape/>
-        </>
+        <div className="pagina-grid">
+            {/* Renderiza o cabeçalho da página */}
+            <Cabecalho />
+
+            {/* Renderiza a mensagem de boas-vindas */}
+            <Welcome />
+
+            {/* Renderiza o rodapé da página */}
+            <Rodape />
+        </div>
     );
 }
 
+// Exporta o componente para ser usado em outras partes do projeto
 export default PHome;
